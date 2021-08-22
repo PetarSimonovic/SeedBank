1e4b3a915440a82106a056ed517919a77dbf0c14
PetarSimonovic <petar@simonovic.com>
2021-08-22 12:02:55 +0100

    Allows users to sign up

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,110 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import api from '../api';
+
+import styled from 'styled-components';
+
+const Title = styled.h1.attrs({
+    className: 'h1',
+})``
+
+const Wrapper = styled.div.attrs({
+    className: 'form-group',
+})`
+    margin: 0 30px;
+`
+
+const Label = styled.label`
+    margin: 5px;
+`
+
+const InputText = styled.input.attrs({
+    className: 'form-control',
+})`
+    margin: 5px;
+`
+
+const Button = styled.button.attrs({
+    className: `btn btn-primary`,
+})`
+    margin: 15px 15px 15px 5px;
+`
+
+const CancelButton = styled.a.attrs({
+    className: `btn btn-danger`,
+})`
+    margin: 15px 15px 15px 5px;
+`
 
 class SignUp extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+        }
+    }
+
+    handleChangeInputName = async event => {
+        const name = event.target.value
+        this.setState({ name })
+    }
+
+    handleChangeInputEmail = async event => {
+        const email = event.target.value
+        this.setState({ email })
+    }
+
+    handleChangeInputPassword = async event => {
+        const password = event.target.value
+        this.setState({ password })
+    }
+
+    handleAddUser = async () => {
+        const { name, email, password } = this.state
+        const payload = { name, email, password }
+
+        await api.addUser(payload).then(res => {
+            window.alert(`User added`)
+            this.setState({
+                name: '',
+                email: '',
+                password: '',
+            })
+        })
+    }
+
     render() {
+        const { name, email, password } = this.state
         return (
-            <div>
-                <p>In this page you'll see the form to sign up</p>
-            </div>
+            <Wrapper>
+                <Title>Sign Up</Title>
+
+                <Label>Name: </Label>
+                <InputText
+                    type="text"
+                    value={name}
+                    onChange={this.handleChangeInputName}
+                />
+
+                <Label>Email: </Label>
+                <InputText
+                    type="text"
+                    value={email}
+                    onChange={this.handleChangeInputEmail}
+                />
+
+                <Label>Password: </Label>
+                <InputText
+                    type="text"
+                    value={password}
+                    onChange={this.handleChangeInputPassword}
+                />
+
+                <Button onClick={this.handleAddUser}>Add User</Button>
+                <CancelButton href={'/movies/list'}>Cancel</CancelButton>
+            </Wrapper>
         )
     }
 }
