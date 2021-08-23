aea51fb588b6c518031b7e8a93a889427cf052ce
PetarSimonovic <petar@simonovic.com>
2021-08-23 18:54:36 +0100

    files tidied up, client index updated

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,11 +1,10 @@
 import Links from './Links'
 import Logo from './Logo'
 import NavBar from './NavBar'
-import SignUpButton from './SignUpButton'
 import Garden from './Garden'
 import GardenPlot from './GardenPlot'
 import Plant from './Plant'
 
 
 
-export { Links, Logo, NavBar, SignUpButton, Garden, GardenPlot, Plant }
+export { Links, Logo, NavBar, Garden, GardenPlot, Plant }
