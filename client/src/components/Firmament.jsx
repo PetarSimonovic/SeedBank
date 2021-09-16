18050628220dee5ac04fc06a422177c55e6cf187
PetarSimonovic <petar@simonovic.com>
2021-09-16 20:29:05 +0100

    sky tweaked

--- client/src/components/Firmament.jsx
+++ client/src/components/Firmament.jsx
@@ -3,15 +3,15 @@ import { Sky } from '@react-three/drei'
 export default function Firmament(props) {
   return (
     <Sky
-      turbidity={5}
-      distance={45000} // Camera distance (default=450000)
-      sunPosition={[900000, 40000, 0]} // Sun position normal (defaults to inclination and azimuth if not set)
-      inclination={0.3} // Sun elevation angle from 0 to 1 (default=0)
-      elevation={1}
-      exposure={0}
-      mieDirectionalG={0.3}
-      rayleigh={0.8}
-      azimuth={0.9} // Sun rotation around the Y axis from 0 to 1 (default=0.25)
+      turbidity={2}
+      distance={450000} // Camera distance (default=450000)
+      sunPosition={[0, 1, 0]} // Sun position normal (defaults to inclination and azimuth if not set)
+      inclination={0.4} // Sun elevation angle from 0 to 1 (default=0)
+      elevation={0.6}
+      exposure={1}
+      mieDirectionalG={0.9}
+      rayleigh={0.08}
+      azimuth={0.6} // Sun rotation around the Y axis from 0 to 1 (default=0.25)
       {...props} // All three/examples/jsm/objects/Sky props are valid
     />  )
 }
